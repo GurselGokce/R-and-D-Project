48cf72cd08d57d7af8da5f148df8d3b56c2751ab
GurselGokce <gursel.gokce@student.ap.be>
2020-03-01 19:33:56 +0100

    Routermodule toegevoegd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RouterModule, Routes, Router} from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { Pagina1Component } from './pagina1/pagina1.component';
+import { Pagina2Component } from './pagina2/pagina2.component';
+import { Pagina3Component } from './pagina3/pagina3.component';
+import { FormsModule }   from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    Pagina1Component,
+    Pagina2Component,
+    Pagina3Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([{ path: "login", component: LoginComponent },
+    { path: "pagina1", component: Pagina1Component},
+    { path: "pagina2", component: Pagina2Component },
+    { path: "pagina3", component: Pagina3Component },
+    { path: "", redirectTo: "login", pathMatch: "full" }])
   ],
   providers: [],
   bootstrap: [AppComponent]
